0cf94e8a730e26e7cfaac739ac6cc01e6a17d8c3
tianyao <ty18710388929@163.com>
2022-07-01 09:02:22 +0800

    * 支持业务代码获取当前是多少次循环结构

--- controller.go
+++ controller.go
@@ -21,7 +21,7 @@ type Controller struct {
 	informer        cache.Controller
 	objType         pkg_runtime.Object
 	businessFunc    func(key string, count int, object interface{}) error
-	loopDoneErrFunc func(err error, key interface{}, obj interface{})
+	loopDoneErrFunc func(err error, count int, key interface{}, obj interface{})
 	requeueTimes    int
 }
 
@@ -31,14 +31,13 @@ func NewController(
 	requeueTimes int,
 	objType pkg_runtime.Object,
 	businessFunc func(key string, count int, object interface{}) error,
-	loopDoneErrFunc func(err error, key interface{}, obj interface{}),
+	loopDoneErrFunc func(err error, count int, key interface{}, obj interface{}),
 	transformer cache.TransformFunc,
 	shouldReSync cache.ShouldResyncFunc,
 	watchErrorHandler cache.WatchErrorHandler,
 	retryOnError bool,
 	watchListPageSize int64,
 ) *Controller {
-
 	// create the workqueue
 	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
@@ -60,6 +59,7 @@ func NewController(
 	return controller
 }
 
+// newResourceEventHandlerFunc 创建包内使用的informerHandler
 func newResourceEventHandlerFunc(queue workqueue.RateLimitingInterface) cache.ResourceEventHandlerFuncs {
 	return cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
@@ -85,95 +85,7 @@ func newResourceEventHandlerFunc(queue workqueue.RateLimitingInterface) cache.Re
 	}
 }
 
-func (c *Controller) processNextItem() bool {
-	// Wait until there is a new item in the working queue
-	key, quit := c.queue.Get()
-	if quit {
-		return false
-	}
-	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
-	// This allows safe parallel processing because two pods with the same key are never processed in
-	// parallel.
-	defer c.queue.Done(key)
-
-	// Invoke the method containing the business logic
-	obj, exists, err := c.indexer.GetByKey(key.(string))
-	if err != nil {
-		klog.Errorf("Fetching object with key %s from store failed with %v", key, err)
-		return false
-	}
-	if !exists {
-		// Below we will warm up our cache with a Pod, so that we will see a delete for one pod
-		fmt.Printf("Pod %s does not exist anymore\n", key)
-	} else {
-		err = c.businessFunc(key.(string), c.queue.NumRequeues(key), obj)
-	}
-
-	// Handle the error if something went wrong during the execution of the business logic
-	c.handleErr(err, key, obj)
-	return true
-}
-
-// handleErr checks if an error happened and makes sure we will retry later.
-func (c *Controller) handleErr(err error, key interface{}, obj interface{}) {
-	if err == nil {
-		// Forget about the #AddRateLimited history of the key on every successful synchronization.
-		// This ensures that future processing of updates for this key is not delayed because of
-		// an outdated error history.
-		c.queue.Forget(key)
-		return
-	}
-
-	// This controller retries 5 times if something goes wrong. After that, it stops trying.
-	if c.queue.NumRequeues(key) < c.requeueTimes {
-		klog.Infof("Error syncing pod %v: %v", key, err)
-
-		// Re-enqueue the key rate limited. Based on the rate limiter on the
-		// queue and the re-enqueue history, the key will be processed later again.
-		c.queue.AddRateLimited(key)
-		return
-	}
-
-	c.queue.Forget(key)
-	// Report to an external entity that, even after several retries, we could not successfully process this key
-	c.loopDoneErrFunc(err, key, obj)
-	klog.Infof("Dropping obj %q out of the queue: %v", key, err)
-}
-
-// Run begins watching and syncing.
-func (c *Controller) Run(workers int, stopCh chan struct{}) {
-	defer runtime.HandleCrash()
-
-	// Let the workers stop when we are done
-	defer c.queue.ShutDown()
-	klog.Infof("Starting %s controller", reflect.TypeOf(c.objType).Elem().Name())
-
-	go c.informer.Run(stopCh)
-
-	// Wait for all involved caches to be synced, before processing items from the queue is started
-	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
-		runtime.HandleError(errors.New(fmt.Sprintf("Timed out waiting for caches to sync %s", c.objType.GetObjectKind())))
-		return
-	}
-
-	for i := 0; i < workers; i++ {
-		go wait.Until(c.runWorker, time.Second, stopCh)
-	}
-
-	<-stopCh
-	klog.Infof("Stopping %s controller", reflect.TypeOf(c.objType).Elem().Name())
-}
-
-func (c *Controller) runWorker() {
-	for c.processNextItem() {
-	}
-}
-
-// NewListWatchFromFunc creates a new ListWatch from the specified func
-func NewListWatchFromFunc(listFunc cache.ListFunc, watchFunc cache.WatchFunc) *cache.ListWatch {
-	return &cache.ListWatch{ListFunc: listFunc, WatchFunc: watchFunc}
-}
-
+// newInformer 创建Infromer
 func newInformer(
 	lw cache.ListerWatcher,
 	objType pkg_runtime.Object,
@@ -254,4 +166,90 @@ func processDeltas(
 		}
 	}
 	return nil
+}
+
+func (c *Controller) processNextItem() bool {
+	// Wait until there is a new item in the working queue
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
+	// This allows safe parallel processing because two pods with the same key are never processed in
+	// parallel.
+	defer c.queue.Done(key)
+
+	// Invoke the method containing the business logic
+	obj, exists, err := c.indexer.GetByKey(key.(string))
+	if err != nil {
+		klog.Errorf("Fetching object with key %s from store failed with %v", key, err)
+		return false
+	}
+	if !exists {
+		// Below we will warm up our cache with a Pod, so that we will see a delete for one pod
+		klog.Infof("Pod %s does not exist anymore\n", key)
+	} else {
+		err = c.businessFunc(key.(string), c.queue.NumRequeues(key), obj)
+	}
+
+	// 执行正常，清空key重新入队等信息
+	if err == nil {
+		// Forget about the #AddRateLimited history of the key on every successful synchronization.
+		// This ensures that future processing of updates for this key is not delayed because of
+		// an outdated error history.
+		c.queue.Forget(key)
+		return true
+	}
+
+	// 重新入队
+	if c.queue.NumRequeues(key) < c.requeueTimes {
+		klog.Infof("Error syncing %v %v: %v", c.objType.GetObjectKind(), key, err)
+
+		// Re-enqueue the key rate limited. Based on the rate limiter on the
+		// queue and the re-enqueue history, the key will be processed later again.
+		c.queue.AddRateLimited(key)
+		return true
+	}
+
+	// 调用业务逻辑，处理达到最大循环次数仍然无法处理的错误
+	c.loopDoneErrFunc(err, c.queue.NumRequeues(key), key, obj)
+	// 超过最大重新入对次数，放弃key处理，调用业务循环结束
+	c.queue.Forget(key)
+
+	klog.Infof("Dropping obj %q out of the queue: %v", key, err)
+	return true
+}
+
+// Run begins watching and syncing.
+func (c *Controller) Run(workers int, stopCh chan struct{}) {
+	defer runtime.HandleCrash()
+
+	// Let the workers stop when we are done
+	defer c.queue.ShutDown()
+	klog.Infof("Starting %s controller", reflect.TypeOf(c.objType).Elem().Name())
+
+	go c.informer.Run(stopCh)
+
+	// Wait for all involved caches to be synced, before processing items from the queue is started
+	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+		runtime.HandleError(errors.New(fmt.Sprintf("Timed out waiting for caches to sync %s", c.objType.GetObjectKind())))
+		return
+	}
+
+	for i := 0; i < workers; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	<-stopCh
+	klog.Infof("Stopping %s controller", reflect.TypeOf(c.objType).Elem().Name())
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+	}
+}
+
+// NewListWatchFromFunc creates a new ListWatch from the specified func
+func NewListWatchFromFunc(listFunc cache.ListFunc, watchFunc cache.WatchFunc) *cache.ListWatch {
+	return &cache.ListWatch{ListFunc: listFunc, WatchFunc: watchFunc}
 }
